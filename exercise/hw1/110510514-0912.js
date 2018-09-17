a7d395eeb26ed9198ecc26e9c0082fbd1e5665c8
110510514 <31942552+110510514@users.noreply.github.com>
2018-09-17 23:23:09 +0800

    Add files via upload

---
+++ exercise/hw1/110510514-0912.js
@@ -0,0 +1,35 @@
+const http = require ('http')
+const fs = require('fs')
+const url = require ('url')
+
+const server = http.createServer (function(req , res) {
+  const path = url.parse (req.url)
+  const filePath = '.' + path.pathname
+  console.log ('filepath=' + filePath )
+  fs.readFile(filePath , 'utf8' , function(err) {
+    switch(req.url){
+            case '/hello' :
+                Text = '你好' ; 
+                break ;
+        
+            case '/name' :
+                Text = '莊奕軒' ; 
+                break ;
+        
+            case '/id' :
+                Text = '110510514' ; 
+                break ;
+            default:
+                res.writeHead(404)
+                res.end()
+
+
+    }
+    res.writeHead(200, {'Content-Type': 'text/html ; charset=utf-8'})
+    res.write(Text)
+    res.end()
+    
+})
+})
+server.listen(3000)
+console.log('Server running at http://localhost:3000')
